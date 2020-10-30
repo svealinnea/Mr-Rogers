8f2c471d88635fa1954225ec18e95035573ba3fe
Svea Wade <svealinneawade@gmail.com>
2020-10-30 10:18:50 -0700

    totally lost. researching

--- js/scripts.js
+++ js/scripts.js
@@ -4,10 +4,9 @@
 //Test 1: "It will return "Beep!" for all numbers that have a 1 in it."
 //Expect: inputNumbers(1).toEqual("Beep!")
 
-function inputNumbers(number) {
-  if (number === 1) {
-    return "beep"
-  }
-}
+//Test 2: "It will return "boop" for all numbers that have a 2 in it."
+//Expect: inputNumbers(2).toEqual("Boop!")
+
+
 
 //UI Logic//
