21808a0142be9145e879e09fcfec35f8af086fb0
Svea Wade <svealinneawade@gmail.com>
2020-10-30 16:18:05 -0700

    final commit, I am stumped

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,22 @@
 //Business Logic//
-function userInput(numbers) {
- 
- 
+function readInput(userInput) {
+  let inputArray = [1,2,3,4,5,6,7,8,9,10];
 
+  if (userInput.indexOf("1")) {
+    for (i =0; i <= userInput; i++) {
+    inputArray.push("beep");
+    }
+  }
+}
+//testing to see if the above works. Does not seem to work. 
 
 //UI Logic//
+$(document).ready(function() {
+  $("form#Rogers").click(function(event){
+    event.preventDefault();
 
 
-//Describe: UserInput()
+//Describe: readInput()
 
 //Test 1: "It will return "Beep!" for all numbers that have a 1 in it."
 //Expect: inputNumbers(1).toEqual("Beep!")
