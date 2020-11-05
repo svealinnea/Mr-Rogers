4ba929de3a0d92207a56c1eafae0b81d2005d346
Svea Wade <svealinneawade@gmail.com>
2020-11-04 18:18:05 -0800

    passed test #2

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,13 @@
 //Business Logic//
 //Test 1
 function readInput(userInput) {
-  return [userInput];
+  let array = [];
+  for (i=0; i<=userInput; i++) {
+    array.push(i);
+  }return array;
 }
+  
+
 
 
 //UI Logic//
@@ -10,14 +15,17 @@ function readInput(userInput) {
 
 
 //Describe: readInput()
-//Test 1: It will return an array.
-//Expect: inputNumbers(5).toEqual("1,2,3,4,5")
+//Test 1: It will identify user input
+//Expect: readInput(1).toEqual(1)
 
-//Test 2: "It will return "Beep!" for all numbers that have a 1 in it."
-//Expect: inputNumbers(1).toEqual("Beep!")
+//Test 2: It will return an array.
+//Expect: readInput(5).toEqual("1,2,3,4,5")
 
-//Test 3: "It will return "boop" for all numbers that have a 2 in it."
-//Expect: inputNumbers(2).toEqual("Boop!")
+//Test 3: "It will return "Beep!" for all numbers that have a 1 in it."
+//Expect: readInput(1).toEqual("Beep!")
 
-//Test 4: "It will return "Won't you be my neighbor? if a 3 is entered"
-//Expect inputNumbers(3).toEqual("Won't you be my neighbor?")
+//Test 4: "It will return "boop" for all numbers that have a 2 in it."
+//Expect: readInput(2).toEqual("Boop!")
+
+//Test 5: "It will return "Won't you be my neighbor? if a 3 is entered"
+//Expect readInput(3).toEqual("Won't you be my neighbor?")
